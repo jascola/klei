953e36b348a720177ae03787618a5b6e0e36670f
jascola <1947857236@qq.com>
2020-04-25 01:03:18 +0800

    Klei

---
+++ DoNotStarveTogether/254076664/Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,27 @@
+return {
+  ["workshop-1154432818"]={ configuration_options={ ChineseLanguage=0, q=0 }, enabled=true },
+  ["workshop-2008480548"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-543945797"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      number_size="default",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  } 
+}
---
+++ DoNotStarveTogetherRail/76561197971398001/Cluster_2/Master/modoverrides.lua
@@ -0,0 +1,30 @@
+return {
+  ["workshop-100010058"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-100010059"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010062"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-100010063"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-2199027653598511647"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-2199027653598516141"]={ configuration_options={ ChineseLanguage=0, q=0 }, enabled=true } 
+}
